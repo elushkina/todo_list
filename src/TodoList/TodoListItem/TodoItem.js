763733dd174dd472bded3112cb61c8be3b8bc433
elushkina <yopolina@gmail.com>
2021-07-22 14:07:37 +0300

    commit

---
+++ src/TodoList/TodoListItem/TodoItem.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './TodoListItem.css'
+
+export const TodoListItem = () => {
+    return (
+        <div className='todoList__item'>
+        <ul>
+            <li>купить молоко</li>
+            <li>купить булочку</li>
+        </ul></div>
+)
+}
